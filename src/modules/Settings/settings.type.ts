07e2afbf1037bc57881fb2ecff874e326f83e347
Nathan Uy <nathaniel.t.uy@gmail.com>
2026-03-13 22:14:29 +0800

    chore:Create settings types

---
+++ src/modules/Settings/settings.type.ts
@@ -0,0 +1,16 @@
+export type ThemeColor = 'terracotta' | 'ocean' | 'forest' | 'lavender' | 'rose'
+export type SettingsTab = 'profile' | 'appearance' | 'notifications' | 'data'
+
+export interface NotificationPreferences {
+  pomodoroEnd: boolean
+  breakEnd: boolean
+  taskDue: boolean
+  dailyReminder: boolean
+  dailyReminderTime: string
+}
+
+export interface AppSettings {
+  darkMode: boolean
+  themeColor: ThemeColor
+  notifications: NotificationPreferences
+}
