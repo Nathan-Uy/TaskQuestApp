0c6a8c65c81db67da8d20e1be27269ceae3fd7cd
Nathan Uy <nathaniel.t.uy@gmail.com>
2026-04-02 20:48:01 +0800

    chore:Register workspace routes to index

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,10 @@ import authRoutes from "./routes/auth.routes";
 import taskRoutes from "./routes/task.routes";
 import goalRoutes from "./routes/goals.routes";
 import aiRoutes from "./routes/ai.routes";
+import workspaceTeamRoutes from "./routes/workspace.team.routes";
+import workspaceSprintRoutes from "./routes/workspace.sprint.routes";
+import workspaceTaskRoutes from "./routes/workspace.task.routes";
+import workspaceChatRoutes from "./routes/workspace.chat.routes";
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
@@ -19,6 +23,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/goals", goalRoutes);
 app.use("/api/ai", aiRoutes);
+app.use("/api/workspace/teams", workspaceTeamRoutes);
+app.use("/api/workspace/sprints", workspaceSprintRoutes);
+app.use("/api/workspace/tasks", workspaceTaskRoutes);
+app.use("/api/workspace/chat", workspaceChatRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI!)
