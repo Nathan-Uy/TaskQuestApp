3dfd9c921b869871a63484971a06af53da0abc8b
Nathan Uy <nathaniel.t.uy@gmail.com>
2026-04-08 22:49:58 +0800

    backend:routes:Add member routes

--- server/src/routes/project.routes.ts
+++ server/src/routes/project.routes.ts
@@ -2,19 +2,23 @@ import { Router } from "express";
 import { protect } from "../middleware/auth";
 import {
   getProjects,
-  getProject,
+  getProjectById,
   createProject,
   updateProject,
   deleteProject,
+  addMember,
+  removeMember,
 } from "../controllers/project.controller";
 
 const router = Router();
 router.use(protect);
 
 router.get("/", getProjects);
+router.get("/:projectId", getProjectById);
 router.post("/", createProject);
-router.get("/:projectId", getProject);
 router.put("/:projectId", updateProject);
 router.delete("/:projectId", deleteProject);
+router.post("/:projectId/members", addMember);
+router.delete("/:projectId/members/:userId", removeMember);
 
 export default router;
